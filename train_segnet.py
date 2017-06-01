817a9fb840aaf98dfc80471e4e5b334914722522
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-06-01 01:12:07 -0500

    Training segnet?

--- train_segnet.py
+++ train_segnet.py
@@ -57,9 +57,11 @@ test_loader = torch.utils.data.DataLoader(
 
 
 model = make_segnet()
+load_ext = False
 
 if osp.exists(args.save):
     print("Loading snapshot...")
+    load_ext = True
     with open(args.save, 'rb') as f:
         state_dict = torch.load(f)
         model.load_state_dict(state_dict)
@@ -71,10 +73,84 @@ else:
         # if layer.startswith('features'):
         state_dict[layer] = vgg_state[layer]
 
-    vgg_layers = vgg_layers[1:][::-1]
-    deconv_layers = [k for k in state_dict if k.startswith('deconv')]
-    for layer, vgg in zip(deconv_layers, vgg_layers):
-        print(state_dict[layer].size(), vgg_state[vgg].size())
-        state_dict[layer] = vgg_state[vgg]
+    # vgg_layers = vgg_layers[1:][::-1]
+    # deconv_layers = [k for k in state_dict if k.startswith('deconv')]
+    # for layer, vgg in zip(deconv_layers, vgg_layers):
+    #     print(state_dict[layer].size(), vgg_state[vgg].size())
+    #     state_dict[layer] = vgg_state[vgg]
 
     model.load_state_dict(state_dict)
+
+optimizer = optim.SGD(model.parameters(), lr=args.lr)
+criterion = nn.BCELoss()
+criterion.size_average = False
+
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, data in enumerate(train_loader):
+        data = Variable(data)
+        # print(data.size())
+        if args.cuda:
+            data = data.cuda()
+        optimizer.zero_grad()
+        recon = model(data)
+        # loss = loss_function(recon_batch, data, mu, logvar)
+        loss = criterion(recon, data)
+        loss.backward()
+        train_loss += loss.data[0]
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.data[0] / len(data)))
+
+    print('\n====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    for data in test_loader:
+        if args.cuda:
+            data = data.cuda()
+        data = Variable(data, volatile=True)
+        # recon_batch, mu, logvar = model(data)
+        recon = model(data)
+        test_loss += criterion(recon, data).data[0]
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}\n'.format(test_loss))
+    return test_loss
+
+
+if __name__ == '__main__':
+    if not load_ext:
+        best_test_loss = None
+    else:
+        best_test_loss = test(0)
+
+    try:
+        for epoch in range(1, args.epochs + 1):
+            train(epoch)
+            test_loss = test(epoch)
+
+            if not best_test_loss or test_loss < best_test_loss:
+                with open(args.save, 'wb') as f:
+                    torch.save(model.state_dict(), f)
+                best_test_loss = test_loss
+
+    except KeyboardInterrupt:
+        print('-' * 89)
+        print('Exiting from training early')
+
+    # Load the best saved model.
+    with open(args.save, 'rb') as f:
+        # model = Net()
+        state_dict = torch.load(f)
+        model.load_state_dict(state_dict)
+
+    test(epoch)
